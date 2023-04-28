ee2dff9c2ab2852223c1abaeb78a4d0811c10cfb
Khan <99924800+UzKhan2@users.noreply.github.com>
2023-04-28 16:44:10 -0400

    Create readinnet.cpp

---
+++ readinnet.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string file = "ibm01.net";
+	string arg1,arg2,arg3;
+	ifstream infile(file);
+	vector<vector<string>> nets;
+	vector<string> tnet;
+	int count = 0;
+
+	if (!infile.is_open())
+	{
+		cout << "Error opening file";
+		return 0;
+	}
+	else
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			infile >> arg1;
+		}
+		
+		//while (!infile.eof())
+		int limit = 0;
+		while (limit < 50)
+		{
+			infile >> arg1;
+			infile >> arg2;
+			if (arg2.compare("s") == 0)	// Tests to see if start of net
+			{
+				infile >> arg3;
+				if (count > 0)
+				{
+					nets.push_back(tnet);
+					tnet.clear();
+				}
+				tnet.push_back(arg1);
+				count++;
+			}
+			else
+			{
+				tnet.push_back(arg1);
+			}
+			limit++;
+		}
+	}
+	for (int i = 0; i < nets.size(); i++)
+	{
+		cout << "Net" << i << " ";
+		for (int j = 0; j < nets[0].size(); j++)
+		{
+			cout << nets[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	/*vector<vector<int> > v = { {1,2}, {3,4} };
+	for (const auto& inner : nets) {
+		for (const auto& item : inner) {
+			cout << item << " ";
+		}
+	}*/
+
+	infile.close();
+	return 0;
+}
