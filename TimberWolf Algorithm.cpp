8a0158f8040f69caf85ad4467393559b8cec9e8e
Arman Syed <96746303+arman0624@users.noreply.github.com>
2023-05-04 19:52:34 -0400

    Update TimberWolf Algorithm.cpp

--- TimberWolf Algorithm.cpp
+++ TimberWolf Algorithm.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int lt; //length perturb range limit
 int ht; //height perturb range limit
+vector <vector<string>> coord; //holds all coordinates for gates and pins
 
 void perturb(vector<vector<string>> chip, int height, int length, vector<int> sda, int numGates, vector<int> areas, int lcd, double T) { //coords, maxh, maxl, scldwnarea, areas.size(), areas, minl, temperature
 	int choice = rand() % 2; //0 or 1 to decide on move or swap
@@ -53,6 +54,7 @@ void perturb(vector<vector<string>> chip, int height, int length, vector<int> sd
 	}
 	lt = lt * (log(tnext) / log(tcurr)); //reduce scope
 	ht = ht * (log(tnext) / log(tcurr));
+	coord = chip;
 }
 
 double schedule(double T)
@@ -71,7 +73,7 @@ int max(vector <int> temp)
 	int max = temp[0];
 	for (int i = 1; i < temp.size(); i++)
 	{
-		if (temp[i] > max) {max = temp[i];}
+		if (temp[i] > max) { max = temp[i]; }
 	}
 	return max;
 }
@@ -81,14 +83,15 @@ int min(vector <int> temp)
 	int min = temp[0];
 	for (int i = 1; i < temp.size(); i++)
 	{
-		if (temp[i] < min) {min = temp[i];}
+		if (temp[i] < min) { min = temp[i]; }
 	}
 	return min;
 }
 
-int cost15(vector<vector<string>> nets, vector <vector<string>> coord, int c)	// Length Cost
+double cost1_5(vector<vector<string>> nets, vector<vector<string>> coord, vector<int> areas, int c)	// Length Cost and Critical Net cost. Determined by c, the select
 {
-	int rangey = 0, rangex = 0, ind = 0, sum = 0, maxNLength = 0;
+	int rangey = 0, rangex = 0, ind = 0, sum = 0;
+	double maxNLength = 0, avgNetLength = 0;
 	vector <int> xcoord;
 	vector <int> ycoord;
 	vector <int> net_length;
@@ -97,25 +100,113 @@ int cost15(vector<vector<string>> nets, vector <vector<string>> coord, int c)	//
 	{
 		for (int j = 0; j < nets[0].size(); j++)
 		{
-			ind = stoi(nets[i][j].substr(1, nets[i][j].size() - 1));
-			xcoord.push_back(stoi (coord[ind][1]));
+			if (nets[i][j].substr(1, nets[i][j].size() - 1).compare("p") == 0) { //if pin
+				ind = stoi(nets[i][j].substr(1, nets[i][j].size() - 1)) + (areas.size()-1);
+			}
+			else {// if gate
+				ind = stoi(nets[i][j].substr(1, nets[i][j].size() - 1));
+			}		
+			xcoord.push_back(stoi(coord[ind][1]));
 			ycoord.push_back(stoi(coord[ind][2]));
 		}
 		rangey = max(ycoord) - min(ycoord);
 		rangex = max(xcoord) - min(xcoord);
 		net_length.push_back(rangey + rangex);
 		sum += rangey + rangex;
+		xcoord.clear();
+		ycoord.clear();
 	}
 	maxNLength = max(net_length);
 	if (c == 1)
 	{
-		return sum / nets.size();
+		avgNetLength = sum / nets.size();
+		return avgNetLength;
 	}
 	else if (c == 5)
 	{
 		return maxNLength;
 	}
+	else {
+		cout << "Invalid select chosen" << endl;
+	}
 }
+
+
+double cost2(vector<vector<string>> coord, vector<int> areas, int minl, int length, int height) {
+	vector<vector<int>> coordsTakenUp; //if 0, free, if 1 then taken up, if 2 taken up by at least 2 cells
+	vector<int> allZeroes;
+	int overlap = 0; //number of coordinates with overlap
+	int numCoords = length * height;
+	int lenOfCell = 0;
+	int y = 0;
+	double percentOfOverlap;
+
+	for (int i = 0; i < length; i++) {
+		allZeroes.push_back(0);
+	}
+	for (int j = 0; j < height; j++) {
+		coordsTakenUp.push_back(allZeroes);
+	}
+
+	for (int k = 0; k < areas.size(); k++) {
+		lenOfCell = areas[k] / minl;
+		y = stoi(coord[k][2]);
+		for (int l = stoi(coord[k][1]); l < lenOfCell; l++) {
+			if (coordsTakenUp[l][y] == 0) {
+				coordsTakenUp[l][y] = 1; //marks as taken
+			}
+			else {
+				coordsTakenUp[l][y] = 2; //marks as at least 2 cells took this coordinate
+			}
+		}
+	}
+
+	for (int i = 0; i < coordsTakenUp.size(); i++) { //counts all 2's. Must do this seperately because if say 3 cells use a coordinate, then an extra increment would occur if i did it in the previous loop. I only want overlap to count how many coordinates have at least 2 cells on it, not how many times it occurs
+		for (int j = 0; j < coordsTakenUp[0].size(); j++) {
+			if (coordsTakenUp[i][j] == 2) {
+				overlap++;
+			}
+		}
+	}
+
+	percentOfOverlap = overlap/numCoords; //% of cells with overlap
+	return percentOfOverlap;
+}
+
+
+
+double cost3(vector<vector<string>> coord, vector<int> areas, int minl, int maxh) {
+	vector<int> xsofrow, rowLens;
+	int minx = 0, maxx = 0, range = 0;
+
+	for (int j = 0; j < maxh; j++) {
+		for (int i = 0; i < areas.size(); i++) {
+			if (stoi(coord[i][2]) == j) {
+				xsofrow.push_back(stoi(coord[i][1]));
+			}
+		}
+		maxx = max(xsofrow);
+		minx = min(xsofrow);
+		range = maxx - minx;
+		rowLens.push_back(range);
+		xsofrow.clear();
+	}
+
+	double avgRowLen = 0;
+	for (int k = 0; k < rowLens.size(); k++) {
+		avgRowLen += rowLens[k];
+	}
+	avgRowLen /= rowLens.size();
+
+	return avgRowLen;
+}
+
+
+int cost4() {
+
+}
+
+
 
 int main()
 {
@@ -138,7 +229,6 @@ int main()
 	vector <int> areas;
 	vector <string> pins;
 	vector <string> names;
-	vector <vector<string>> coord;
 	vector <string> tcoord;
 	vector <int> scldwnarea;
 	vector <string> pins2;
